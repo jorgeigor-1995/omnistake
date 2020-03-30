888df41524ab4e12ca32257ed5def391c823feb9
jorgeigor <jorgeigor.1995@gmail.com>
2020-03-29 22:25:53 -0300

    back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json);
+app.use(routes);
+
+/** 
+ * Rotas / Recurso
+*/
+
+/**
+ * Método http
+ * 
+ * GET: Busca informação do back-end
+ * POST: Criar uma informação do back-end
+ * PUT: Alterar uma informação
+ * DETELE: Deletar uma informação
+ */
+
+ /**
+  * Tipos de parametros:
+  * 
+  * Query: Parametros nomeados na rota após (?) (filtros, paginação)
+  * Route Params: Parametros utilizados para identificar recursos
+  * Request Body: Corpo da requisição, utilizado para criar ou alterar 
+  */
+
+  /**
+   * SQL: MySQL, SQLite, PostGreSql, Oracle, SQL Server
+   * NoSQL: Mongo, CouchBD
+   */
+
+   /**
+    * Driver: select * from users
+    * query builder: table('users).select('*').where
+    */
+
+
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
